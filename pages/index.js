0f8e3b924a0b0f5215c559956829e0c5f7ec0508
Hamza09Hamza <94070067+Hamza09Hamza@users.noreply.github.com>
2022-12-03 22:47:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Slider from '../Slider.jsx'
+    import React from 'react'
+import { useState } from 'react'
+import { eventObj } from '../SliderFunction'
+import styles from "../Slider.module.css"
 
 export default function Home() {
+  const [eventName, setEventName] = useState("stopped")
+    const [Array, setArray] = useState(["red", "blue", "green", "yellow", "purple"])
+
+    setTimeout(() => {
+        if (eventName === "stopped") {
+            setEventName("push")
+        }
+        else if (eventName === "push") {
+            setEventName("change")
+        }
+        else if (eventName === "change") {
+
+            setEventName("stopped");
+            let Arr = Array;
+            const FirstItem = Arr.shift();
+            Arr.push(FirstItem);
+            setArray(Arr);
+        }
+    }, 9000)
   return (
     <>
-    <Slider/>
-  </>)
+      <div className={styles.divSlider}>
+            {Array.map((item) => {
+                const ItemPlace = Array.indexOf(item)
+                return (<>
+                    <div
+                        style={{ backgroundColor: item }}
+                        className={styles.itemSlider + " " + eventObj(eventName, ItemPlace)}
+                    >
+                        <h1> Its here The  {item}</h1>
+                    </div>
+                </>)
+            })
+            }
+
+        </div>
+    </>);
 }
+
+
+
